bb55145430066d66d94032b761fffef222cbc3d4
rutujaa121 <rutujaaugale@gmail.com>
2025-10-07 23:27:00 -0700

    added

--- 02-slip/q2.php
+++ 02-slip/q2.php
@@ -1,37 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="q2.php" method="POST">
-        
-        Enter choice <input type="text" name="c"><br>
-        1.Insert an Element in Queue <br><br>
-        2. Delete an element from Queue <br><br>
-        3. Display the contents of Queue <br><br>
+        Enter First String <input type="text" name="str1"><br><br>
+        Enter Second String <input type="text" name="str2"><br><br>
+
+        Enter Position <input type="text" name="pos"><br><br>
+        Enter no. of Characters <input type="text" name="num"><br><br>
+
+        <input type="radio" name="option" value="1"> Delete a small part <br>
+        <input type="radio" name="option" value="2"> Insert the small string <br>
+        <input type="radio" name="option" value="3"> Replace some characters <br>
+        <input type="radio" name="option" value="4"> Replace all the characters <br>
+
         <input type="submit" value="Submit">
     </form>
 </body>
+
 </html>
 
 <?php
-    $ch = $_POST["c"];
-    $a = array(10,20,30);
+$a = $_POST['str1'];
+$b = $_POST['str2'];
+$pos = $_POST['pos'];
+$num = $_POST['num'];
+$ch = $_POST['option'];
 
-    switch ($ch)
-    {
-        case 1 : $c=array_unshift($a, 60);
-        print_r($c);
-        break;
+switch($ch)
+{
+    case 1: $pos=substr_replace($a,$b,$pos,$num);
+    if($pos!=0)
+            echo "stirng are deleted";
+        else
+            echo "string are not deleted";
+            break;
 
-        case 2: $c=array_shift($a);
-        print_r($a);
-        break;
+    case 2: $str=substr_replace($a,$b,$pos,0);
+            echo "After Inserting the small string : ".$str;
+            break;
 
-        case 3: print_r($a);
-        break;
-    }
+    case 3: $str=substr_replace($a,$b,$pos,$num);
+            echo "After Replacing some characters : ".$str;
+            break;
+        
+    case 4: $str=str_replace($a,$b,$a);
+            echo "After Replacing all the characters : ".$str;
+            break;
+}
 ?>
