bd3b490f2078b2ec17ac2b55538add05df2e5061
rutujaa121 <rutujaaugale@gmail.com>
2025-10-09 07:52:31 -0700

    added

---
+++ practice.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="practice.php" method="POST">
+        Enter first string<input type="text" name="s1"><br><br>
+        Enter second string<input type="text" name="s2"><br><br>
+
+        Enter position<input type="text" name="p"><br><br>
+        Enter no. of characters<input type="text" name="n"><br><br>
+
+        <input type="radio" name="opt" value="1">Delete a small part<br>
+        <input type="radio" name="opt" value="2">Insert the small string<br>
+        <input type="radio" name="opt" value="3">Replace some characters<br>
+        <input type="radio" name="opt" value="4">Replace all the characters<br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
+<?php
+    $a = $_POST['s1'];
+    $b = $_POST['s2'];
+    $pos = $_POST['p']; 
+    $num = $_POST['n'];
+    $ch = $_POST['opt'];
+
+    switch($ch)
+    {
+        case 1:
+            $pos = substr_replace($a, $b, $pos, $num);
+            if($pos != 0)
+                echo "string are deleted";
+            else
+                echo "string are not deleted";
+            break;
+        
+        case 2:
+            $str = substr_replace($a, $b, $pos, 0);
+            echo "After Inserting the small string : ".$str;
+            break;
+
+        case 3:
+            $str = substr_replace($a, $b, $pos, $num);
+            echo "After Replacing some characters : ".$str;
+            break;
+
+        case 4:
+            $str = str_replace($a, $b, $a);
+            echo "After Replacing all the characters : ".$str;
+            break;
+    }
+?>
